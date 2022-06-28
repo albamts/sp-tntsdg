6ad70a0299db263e0b30c905c5ac193525276972
Matias Albarracin <mekkir@gmail.com>
2022-06-28 00:17:58 -0300

    js aun sin terminar

---
+++ tareajavascrip.js
@@ -0,0 +1,27 @@
+var distancia = 0;
+//con if
+if(distancia>100000){
+    alert("Viaje en avión.");
+}else if(distancia > 30000){
+    alert("Viaje en auto.");
+}else if(distancia>10000){
+    alert("Viaje en colectivo.");
+}else if(distancia>1000){
+    alert("Viaje en bicicleta.");
+}else if(distancia>0){
+    alert("Viaje a pie.");
+}else{
+    alert("Ingrese solo valores postivos.");
+}
+//con switch??
+//no tengo idea como hacerlo con switch, no cre oque se pueda
+
+var num = [54, 2, 21, 89, 5, 34, 27]
+var n = 0
+var o = 1
+
+while(num[n]>num[o]){
+    o=o+1;
+}
+//voy a tener que esperar a la clase y luego seguir, este me mató
+//no se como sacarle el resultado que necesito...
