fff721dc629aabc1d6cccb8f3d42ba7269ecef00
Matias Albarracin <mekkir@gmail.com>
2022-07-01 12:39:53 -0300

    aun sin exito en js

--- tareajavascrip.js
+++ tareajavascrip.js
@@ -1,5 +1,6 @@
 var distancia = 0;
 //con if
+distancia=prompt('Ingrese la distancia a recorrer en metros:', '0' )
 if(distancia>100000){
     alert("Viaje en avión.");
 }else if(distancia > 30000){
@@ -13,15 +14,30 @@ if(distancia>100000){
 }else{
     alert("Ingrese solo valores postivos.");
 }
-//con switch??
-//no tengo idea como hacerlo con switch, no cre oque se pueda
 
-var num = [54, 2, 21, 89, 5, 34, 27]
+
+var num = [54, 2, 21, 89, 5, 34, 99, 27]
 var n = 0
 var o = 1
 
-while(num[n]>num[o]){
-    o=o+1;
+for(n=0; num[n-1]<num[n]>num[n+1]; n++){
+    n++
 }
+
+document.write('El numero mas alto es ' + num[o]);
+
+
+while(num[n]>num[o]){
+    o++;
+}
+
+
+while(0<=n>num.length-1){
+    for(n=0; num[n-1]<num[n]>num[n+1]; n++){
+
+    }
+}
+document.write('El numero mas alto es ' + num[o]);
+
 //voy a tener que esperar a la clase y luego seguir, este me mató
 //no se como sacarle el resultado que necesito...
