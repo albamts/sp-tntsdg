ad4417a537b7dc99a4fc139fdc87cee4b39f220c
Matias Albarracin <mekkir@gmail.com>
2022-09-16 23:42:18 -0300

    tarea m3u4

--- m3t/app.js
+++ m3t/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 
 var indexRouter = require('./routes/index');
 
@@ -10,8 +11,10 @@ var indexRouter = require('./routes/index');
 var productosRuta = require('./routes/productos');
 var requisitosRuta = require('./routes/requisitos');
 var pedidoRuta = require('./routes/pedido');
+var perfilRuta = require('./routes/perfil');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,16 +26,74 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(session({
+  secret: 'aslkj23poeru432i777wmdla998ssccv8123a',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// app.use('/', indexRouter);
+
+///para contar vistas - visitas? funciona en
+// sesion y leugo reinicia
+app.use(function(req, res, next) {
+  if (!req.session.visita) {
+    req.session.visita = {};
+  }
+  if (!req.session.visita[req.originalUrl]){
+    req.session.visita[req.originalUrl] = 1;
+  } else {
+    req.session.visita[req.originalUrl]++;
+  }
+
+  next();
+});
+
+app.get('/', function(req, res) {
+  var afiliado = Boolean(req.session.quien);
+
+  res.render('index', {
+    title: 'Inicio',
+    afiliado: afiliado,
+    quien: req.session.quien
+  });
+});
+
+// hasta aprender a hacer logins
+app.get('/perfil', function(req, res) {
+  var afiliado = Boolean(req.session.quien);
+
+  res.render('perfil', {
+    title: 'Perfil',
+    afiliado: afiliado,
+    quien: req.session.quien,
+    visita: req.session.visita[req.originalUrl]
+  });
+});
+// -------------------
+
+app.post('/ingreso', function(req, res) {
+  if (req.body.quien) {
+    req.session.quien = req.body.quien
+  }
+  res.redirect('/');
+});
+
+app.get('/terminar', function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+});
 
 //mas propios
 app.use('/productos', productosRuta);
 app.use('/requisitos', requisitosRuta);
 app.use('/pedido', pedidoRuta);
+app.use('/perfil', perfilRuta);
 
 app.use('/fin', function(req,res){
   res.send('<h1>Fin</h1><p><a href="/">........?</a></p>')
 })
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
